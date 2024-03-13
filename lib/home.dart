bcb46fc9de940c30a07d3c832eeb9742f7542cd5
Dipu Ray <dipuray7779@gmail.com>
2024-03-13 21:06:03 +0600

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,85 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:ui_exam17/menu.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(padding: EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(gradient:
+       LinearGradient(colors:
+       [
+         Color.fromARGB(255, 71, 238, 5),
+         Color.fromARGB(255, 217, 255, 0),
+       ],
+       
+       begin: Alignment.topCenter,end: Alignment.bottomCenter
+       ),
+       
+       ),
+      child: Column(
+        children: [
+          Expanded(flex: 2,
+            child:
+          ListTile(title: Text("FINTIMES",style: myStyle(22, Colors.black,FontWeight.w500),),
+          trailing: IconButton(onPressed: (){
+       Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Menue()));
+          }, icon: Icon(Icons.menu)),),
+           ),
+           Expanded(flex: 4,
+             child: Container(padding: EdgeInsets.symmetric(vertical: 40,horizontal: 20),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Everything",style: myStyle(32, Color.fromARGB(255, 133, 138, 138),FontWeight.w700),),
+                  Text("about investing plus way more.",style: myStyle(32, Colors.black,FontWeight.w700),),
+                ],
+              ),
+             ),
+           ),
+           Expanded(
+            child: Container(color: Colors.black,
+            child: Row(children: [
+              Expanded(flex: 5,
+                child: Container(
+                  alignment: Alignment.center,
+                  height: double.infinity,
+                margin: EdgeInsets.all(5),
+                color: Color.fromARGB(255, 251, 255, 4,),child: Text("Become our member",style: myStyle(22, Colors.black,FontWeight.w500,),),
+                
+                )),
+              Expanded(
+                flex: 2,
+                child: Container(height: double.infinity,
+                  margin: EdgeInsets.only(top: 5,right: 5,bottom: 5),
+                  color:Colors.white,
+                  child: Icon(Icons.add,size: 50,),
+                  ),
+                  
+                  ),
+            ],),
+           ))
+        ],
+      ),
+    ),
+    );
+  }
+}
+myStyle(double size,Color clr,[FontWeight? fw]){
+  
+  return TextStyle(
+    fontSize: size,
+    color: clr,
+    fontWeight: fw
+  );
+}
---
+++ lib/menu.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+import 'package:ui_exam17/home.dart';
+
+class Menue extends StatefulWidget {
+  const Menue({super.key});
+
+  @override
+  State<Menue> createState() => _MenueState();
+}
+
+class _MenueState extends State<Menue> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+      child:     
+      Column(
+        children: [
+          Expanded(flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.only(bottom: 5),
+              color: Color.fromARGB(255, 64, 253, 70),
+              child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+           ListTile(
+              title: Text("FINTIMES",style: myStyle(22, Colors.black,FontWeight.w500),),
+              trailing: IconButton(onPressed: (){
+                }, icon: Icon(Icons.menu)),
+            ),
+            SizedBox(height:20),
+                  Expanded(
+                    child: Container(padding: EdgeInsets.all(4),
+                    
+                              color: Colors.black,
+                              child: Text("Top App'22",style: myStyle(18, Colors.white),)),
+                  ),SizedBox(height: 20,),
+            RichText(
+            text: TextSpan(
+              text: 'Introducing the first ',
+              style: myStyle(22, Colors.black),
+              children: const <TextSpan>[
+                TextSpan(text: 'all-in-one', style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(text: ' to help youput your financial assets inteh best possible opportunities!'),
+              ],
+            ),
+          )
+          
+                  ],),),
+          ),
+         Expanded(
+          flex: 2,
+          child: Container(
+             margin: EdgeInsets.only(bottom: 5),
+              color: Color.fromARGB(255, 64, 253, 70),
+            padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Fit Times",style: myStyle(26, Colors.black),),
+                  CircleAvatar(child: Icon(Icons.play_arrow,size: 26,))
+                ],
+              ),
+              SizedBox(height: 30,),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Investment proces",style: myStyle(22, Colors.black),),
+                  Icon(Icons.arrow_forward,size: 26,)
+                ],
+              ),
+            ],
+          ),
+         )),
+         Expanded(
+          flex: 2,
+          child: Container(
+             margin: EdgeInsets.only(bottom: 5),
+              color: Color.fromARGB(255, 64, 253, 70),
+            padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Strategy",style: myStyle(26, Colors.black),),
+                  Stack(
+                  children: [
+                    Positioned(
+                      left: 25,
+                      child: CircleAvatar(maxRadius: 20,backgroundColor: Colors.teal,
+                      child: CircleAvatar(maxRadius: 16,backgroundColor: Colors.white,
+                        child: Icon(Icons.menu),
+                      ),
+                    )),
+                    CircleAvatar(maxRadius: 20,backgroundColor: Colors.teal,
+                      child: CircleAvatar(maxRadius: 16,backgroundColor: Colors.white,
+                        child: Icon(Icons.menu),
+                      ),
+                    ),
+                    SizedBox(width: 70,)
+                    
+                  ],
+                 )
+                ],
+              ), SizedBox(height: 30,),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("CHECK OUR",style: myStyle(22, Colors.black),),
+                  Icon(Icons.arrow_forward,size: 26,)
+                ],
+              ),
+            ],
+          ),
+         )),
+         Expanded(
+          flex: 2,
+          child: Container(
+         
+              color: Color.fromARGB(255, 64, 253, 70),
+            padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Fit Times",style: myStyle(26, Colors.black),),
+                  
+                ],
+              ),
+              SizedBox(height: 30,),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Investment proces",style: myStyle(22, Colors.black),),
+                  Icon(Icons.arrow_forward,size: 26,)
+                ],
+              ),
+            ],
+          ),
+         )),
+         
+                 ],
+      ), 
+      ))
+  
+    ;
+  }
+}
